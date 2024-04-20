f76413e5ee86599077214e2679afafd575491156
Kostian Oleg <159447875+PaRaDoXfour@users.noreply.github.com>
2024-04-21 01:33:57 +0300

    Task3.py

--- Task3.py
+++ Task3.py
@@ -1,11 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
-print("Population of children 7-11 ages in Albania for 15 years: ")
 x = ["David", "Mike", "John", "Bob", "Emily", "Tom", "Eve", "Alice", "Sophia", "Sara"]
 y = [190, 185, 180, 175, 173, 172, 170, 168, 162, 160]
 np.array(x)
 np.array(y)
 fig, ax = plt.subplots()
+plt.title('Student growth')
 ax.pie(y, labels = x)
 ax.axis("equal")
 plt.legend()
