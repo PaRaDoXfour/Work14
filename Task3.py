aaacb6c9bee356d3c4e5579e246935e4e75d2f88
Kostian Oleg <159447875+PaRaDoXfour@users.noreply.github.com>
2024-04-21 01:40:46 +0300

    Task3.py

--- Task3.py
+++ Task3.py
@@ -1,12 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+# Створюємо списки з іменами студентів та їхнім зростом
 x = ["David", "Mike", "John", "Bob", "Emily", "Tom", "Eve", "Alice", "Sophia", "Sara"]
 y = [190, 185, 180, 175, 173, 172, 170, 168, 162, 160]
+
+# Хоча тут створюються NumPy масиви, вони не використовуються в подальшому коді.
 np.array(x)
 np.array(y)
+
+# Створення об'єкта графіку
 fig, ax = plt.subplots()
-plt.title('Student growth')
-ax.pie(y, labels = x)
+
+# Надаємо назву графіку
+plt.title('Student Growth')
+
+# Створюємо кругову діаграму
+ax.pie(y, labels=x)
+
+# Встановлюємо, щоб осі були однаково масштабовані, тобто круг має бути кругом, а не еліпсом
 ax.axis("equal")
+
+# Вивід легенди, що допомагає ідентифікувати, який сектор відповідає якому студенту
 plt.legend()
+
+# Показуємо графік
 plt.show()
